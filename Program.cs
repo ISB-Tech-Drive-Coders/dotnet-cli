30ba1f7a4cc36d0a7207c9ac29a4c15872f3040d
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2022-08-28 16:04:57 -0700

    Organize a bit more

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace sudopswrd
             Console.WriteLine("What is this PC's password? (Needed for sudo commands)");
             //sudopswrd is the password to be used for sudo commands
             var sudopswrd = Console.ReadLine();
+
+
+            
             //The code lower then this is for the end of the script to exit
             Console.WriteLine("Thank you for using the ISBTD Windows Configurator");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
