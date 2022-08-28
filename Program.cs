b1ed050f1ba33d98ef896936df5d3c35e7052014
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2022-08-28 11:34:18 -0700

    Add sudo password collecter and exit method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thank you for using the .Net ISB Tech Drive Configurator v1.0.3 ");
+            Console.WriteLine("What is this PC's password?");
+            //sudopswrd is the password to be used for sudo commands
+            var sudopswrd = Console.ReadLine();
+            //The code lower then this is for the end of the script to exit
+            Console.WriteLine("Thank you for using the ISB Tech Drive .Net CLI Configurator v1.0.3");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
