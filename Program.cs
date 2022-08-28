fa61bcb1757e74ae7e56353e2a89411b2c6ec77a
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2022-08-28 11:40:04 -0700

    Simpler naming

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Thank you for using the .Net ISB Tech Drive Configurator v1.0.3 ");
-            Console.WriteLine("What is this PC's password?");
+            Console.WriteLine("Thank you for using the ISBTD Windows Configurator");
+            Console.WriteLine("What is this PC's password? (Needed for sudo commands)");
             //sudopswrd is the password to be used for sudo commands
             var sudopswrd = Console.ReadLine();
             //The code lower then this is for the end of the script to exit
-            Console.WriteLine("Thank you for using the ISB Tech Drive .Net CLI Configurator v1.0.3");
+            Console.WriteLine("Thank you for using the ISBTD Windows Configurator");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
         }
